8b2e89481488d26ebf94d50ce55b9dbd20ec0e03
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2023-11-13 17:53:16 +0700

    Add Endpoint Ping

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/ping", (req, res) => {
-  res.json({ message: "Ping!" });
+app.get('/ping', (req, res) => {
+  res.json({ message: 'Ping!' });
 });
 
-app.get("/version", (req, res) => {
-  res.json({ version: "1.0.0" });
+app.get('/version', (req, res) => {
+  res.json({ version: '1.0.0' });
 });
 
 // Menjalankan server
