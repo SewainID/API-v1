5a60cfdf1bffe3ba3bd9d50c669e17a9c64ce8ea
hellsing032 <saidina.065121083@unpak.ac.id>
2023-12-02 14:34:10 +0700

    update little bit

--- models/catalogs.js
+++ models/catalogs.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Catalogs extends Model {
+  class catalogs extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Catalogs.init({
+  catalogs.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     size: DataTypes.STRING,
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     day_maintenance: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Catalogs',
+    modelName: 'catalogs',
   });
-  return Catalogs;
+  return catalogs;
 };
