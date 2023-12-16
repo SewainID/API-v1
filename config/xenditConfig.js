4254e820633ddd74ef5bb89c805136cb1a5ef511
hellsing032 <saidina.065121083@unpak.ac.id>
2023-12-17 02:04:48 +0700

    update xenditconfig

--- config/xenditConfig.js
+++ config/xenditConfig.js
@@ -2,7 +2,7 @@ const xenditClient = require('xendit-node');
 
 const xendit = new xenditClient({
     secretKey: process.env.XENDIT_API_KEY ||
-    'xnd_development_...',
+    'xnd_public_development_sbXID2gxRaCfgcy5bOINsQ36taLYTA1H8ZfwQx2gBeL47JT3PnSQRnmQY5Y',
 });
 
 module.exports = xendit;
